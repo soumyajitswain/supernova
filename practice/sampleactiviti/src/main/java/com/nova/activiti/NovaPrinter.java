18e3d7e920a604b71913de627d187a614f69af7b
soumyajitswain <soumyajit.swain@gmail.com>
2014-04-15 21:08:28 +0530

    Initial release - sample test project on activiti

---
+++ practice/sampleactiviti/src/main/java/com/nova/activiti/NovaPrinter.java
@@ -0,0 +1,10 @@
+package com.nova.activiti;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NovaPrinter {
+	 public void printMessage() {
+		    System.out.println("hello world part2");
+		  }
+}
---
+++ practice/sampleactiviti/src/test/java/com/nova/activiti/ActivitiAPITest.java
@@ -0,0 +1,21 @@
+package com.nova.activiti;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"/nova-config-test.xml"})
+public class ActivitiAPITest {
+
+	@Autowired
+	private ActivitiService service;
+	@Test
+	public void runActiviti() {
+		service.deployProcessFiles("hello.bpmn20.xml");
+		service.startProcessInstance("helloProcess");
+	}
+	
+}
